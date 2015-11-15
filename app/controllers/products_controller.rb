c36001752e0750b071232ff39551d5f44b31e958
Bejan Maljai <bmaljai@gmail.com>
2015-11-15 11:32:14 -0800

    added dismissible boxes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,43 @@
 class ProductsController < ApplicationController
-  def productpage
+  def index
     @allproducts = Product.all
+
   end
+  
+  def new
+  end
+
+  def create
+    Product.create(name: params[:name], price: params[:price], description: params[:description], image: params[:image], stock: params[:stock])
+    flash[:success] ="Product Created"
+    redirect_to "/products"
+
+  end
+
+  def show
+    @fetch_product = Product.find_by(id: params[:id])
+  end
+
+  def edit
+    @product = Product.find_by(id: params[:id])
+  end
+
+  def update
+    Product.find_by(id: params[:id]).update(id: params[:id], name: params[:name], price: params[:price], description: params[:description], image: params[:image], stock: params[:stock])
+    flash[:Warning] ="Product Updated"
+    redirect_to "/products"
+
+  end
+
+  def destroy
+      id = params[:id]
+    product = Product.find_by(id: id)
+    product.destroy
+    flash[:warning] ="Product Deleted"
+    redirect_to "/products"
+  
+  end
+
+
 end
+
