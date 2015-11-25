e5c61c9cd0c50290c9eafc6f29b36232cf97061b
Bejan Maljai <bmaljai@gmail.com>
2015-11-24 21:05:59 -0800

    setup for postgresql

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,66 @@
+User.create!([
+  {email: "bmaljai@gmail.com", encrypted_password: "$2a$10$enP0.XaKc9ITTX9eSawMAOOukhBGNW0oC7KrUEBR2uYAmhHcCSVfW", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 3, current_sign_in_at: "2015-11-22 23:46:29", last_sign_in_at: "2015-11-22 23:31:52", current_sign_in_ip: "::1", last_sign_in_ip: "::1"},
+  {email: "bejan.maljai@gmail.com", encrypted_password: "$2a$10$yODH3cm2VWewot3oaUfccuJgNAQR98r/Ymg9vODc2EV//sh7677ym", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2015-11-23 00:48:44", last_sign_in_at: "2015-11-23 00:48:44", current_sign_in_ip: "::1", last_sign_in_ip: "::1"}
+])
+Image.create!([
+  {url: "http://www.motus.com/wp-content/themes/motus/images/default.jpg", name: "default", description: "default image to display", dimensions: "500x500", size: 1000, product_id: 1},
+  {url: "http://www.motus.com/wp-content/themes/motus/images/default.jpg", name: "default", description: "default image to display", dimensions: "500x500", size: 1000, product_id: 2},
+  {url: "http://www.motus.com/wp-content/themes/motus/images/default.jpg", name: "default", description: "default image to display", dimensions: "500x500", size: 1000, product_id: 3},
+  {url: "http://www.motus.com/wp-content/themes/motus/images/default.jpg", name: "default", description: "default image to display", dimensions: "500x500", size: 1000, product_id: 4},
+  {url: "http://www.motus.com/wp-content/themes/motus/images/default.jpg", name: "default", description: "default image to display", dimensions: "500x500", size: 1000, product_id: 1},
+  {url: "http://www.motus.com/wp-content/themes/motus/images/default.jpg", name: "default", description: "default image to display", dimensions: "500x500", size: 1000, product_id: 4},
+  {url: "http://www.motus.com/wp-content/themes/motus/images/default.jpg", name: "default", description: "default image to display", dimensions: "500x500", size: 1000, product_id: 2},
+  {url: "http://www.motus.com/wp-content/themes/motus/images/default.jpg", name: "default", description: "default image to display", dimensions: "500x500", size: 1000, product_id: 5},
+  {url: "http://www.motus.com/wp-content/themes/motus/images/default.jpg", name: "default", description: "default image to display", dimensions: "500x500", size: 1000, product_id: 6},
+  {url: "http://www.motus.com/wp-content/themes/motus/images/default.jpg", name: "default", description: "default image to display", dimensions: "500x500", size: 1000, product_id: 1},
+  {url: "http://www.motus.com/wp-content/themes/motus/images/default.jpg", name: "default", description: "default image to display", dimensions: "500x500", size: 1000, product_id: 4},
+  {url: "http://www.motus.com/wp-content/themes/motus/images/default.jpg", name: "default", description: "default image to display", dimensions: "500x500", size: 1000, product_id: 3},
+  {url: "http://www.motus.com/wp-content/themes/motus/images/default.jpg", name: "default", description: "default image to display", dimensions: "500x500", size: 1000, product_id: 10},
+  {url: "http://www.motus.com/wp-content/themes/motus/images/default.jpg", name: "default", description: "default image to display", dimensions: "500x500", size: 1000, product_id: 2},
+  {url: "http://www.motus.com/wp-content/themes/motus/images/default.jpg", name: "default", description: "default image to display", dimensions: "500x500", size: 1000, product_id: 1},
+  {url: "http://www.motus.com/wp-content/themes/motus/images/default.jpg", name: "default", description: "default image to display", dimensions: nil, size: nil, product_id: 7},
+  {url: "http://www.motus.com/wp-content/themes/motus/images/default.jpg", name: "default", description: "default image to display", dimensions: nil, size: nil, product_id: 8},
+  {url: "http://www.motus.com/wp-content/themes/motus/images/default.jpg", name: "default", description: "default image to display", dimensions: nil, size: nil, product_id: 9},
+  {url: "http://www.motus.com/wp-content/themes/motus/images/default.jpg", name: "default", description: "default image to display", dimensions: nil, size: nil, product_id: 11},
+  {url: "http://www.motus.com/wp-content/themes/motus/images/default.jpg", name: "default", description: "default image to display", dimensions: nil, size: nil, product_id: 12},
+  {url: "http://www.motus.com/wp-content/themes/motus/images/default.jpg", name: "default", description: "default image to display", dimensions: nil, size: nil, product_id: 13},
+  {url: "http://www.motus.com/wp-content/themes/motus/images/default.jpg", name: "default", description: "default image to display", dimensions: nil, size: nil, product_id: 14},
+  {url: "http://www.motus.com/wp-content/themes/motus/images/default.jpg", name: "default", description: "default image to display", dimensions: nil, size: nil, product_id: 15},
+  {url: "http://www.motus.com/wp-content/themes/motus/images/default.jpg", name: "default", description: "default image to display", dimensions: nil, size: nil, product_id: 16},
+  {url: "http://www.motus.com/wp-content/themes/motus/images/default.jpg", name: "default", description: "default image to display", dimensions: nil, size: nil, product_id: 17},
+  {url: "http://www.motus.com/wp-content/themes/motus/images/default.jpg", name: nil, description: nil, dimensions: nil, size: nil, product_id: 18},
+  {url: "https://git.reviewboard.kde.org/media/uploaded/files/2015/07/18/a70d8ab6-1bbf-4dcc-b11f-524c2f56b14a__picture_default_cover.jpg", name: "default", description: nil, dimensions: nil, size: nil, product_id: 19},
+  {url: "https://git.reviewboard.kde.org/media/uploaded/files/2015/07/18/a70d8ab6-1bbf-4dcc-b11f-524c2f56b14a__picture_default_cover.jpg", name: "default", description: nil, dimensions: nil, size: nil, product_id: 20}
+])
+Order.create!([
+  {user_id: 2, product_id: 1, quantity: 3, subtotal: "50.05", tax: "4.54", total: "0.0"},
+  {user_id: 2, product_id: 2, quantity: 50, subtotal: "29.95", tax: "2.61", total: "31.61"},
+  {user_id: 2, product_id: 2, quantity: 5, subtotal: "29.95", tax: "2.61", total: "31.61"}
+])
 Product.create!([
-  {name: nil, price: nil, description: nil, image: nil, stock: nil, rating: nil},
-  {name: "", price: nil, description: "", image: "", stock: "", rating: nil},
-  {name: "new product 1", price: "100.0", description: "100", image: "", stock: "100", rating: nil},
-  {name: "new new new", price: "99.0", description: "new", image: "", stock: "99", rating: nil},
-  {name: "item 1", price: "3.0", description: "", image: "", stock: "", rating: nil},
-  {name: "item 1", price: "3.0", description: "", image: "", stock: "", rating: nil},
-  {name: "1", price: "5.0", description: "", image: "", stock: "", rating: nil},
-  {name: "1", price: "1.0", description: "", image: "", stock: "", rating: nil},
-  {name: "55", price: "55.0", description: "", image: "", stock: "", rating: nil},
-  {name: "555", price: "555.0", description: "", image: "", stock: "", rating: nil},
-  {name: "item 1", price: "1.0", description: "", image: "", stock: "", rating: nil},
-  {name: "item 2", price: "2.5", description: "", image: "", stock: "", rating: nil},
-  {name: "item 3", price: "0.99", description: "", image: "", stock: "", rating: nil},
-  {name: "item 4", price: "2.5", description: "", image: "", stock: "", rating: nil},
-  {name: "item 5", price: "1.2", description: "", image: "", stock: "", rating: nil},
-  {name: "new item", price: "5.99", description: "this is a new item that i just created with the new models.", image: "", stock: "20", rating: 5},
-  {name: "pro", price: "20.0", description: "pro", image: "", stock: "20", rating: 5}
+  {name: "Chocolate", price: "50.0", description: "this is an item description. this is important additional information.", stock: "1", rating: 5, supplier_id: 1, user_id: 1},
+  {name: "new product 90", price: "29.95", description: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat", stock: "2", rating: 4, supplier_id: 2, user_id: 2},
+  {name: "new product 1", price: "100.0", description: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequatthis is the item description", stock: "100", rating: 5, supplier_id: 1, user_id: 2},
+  {name: "new new new", price: "99.0", description: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequatnew", stock: "99", rating: 3, supplier_id: 2, user_id: 1},
+  {name: "item 11", price: "3.0", description: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat", stock: "14", rating: 4, supplier_id: 1, user_id: 2},
+  {name: "item 21", price: "56.25", description: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat", stock: "34", rating: 1, supplier_id: 2, user_id: 1},
+  {name: "blah blah blah", price: "15.0", description: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat", stock: "21", rating: 2, supplier_id: 1, user_id: 2},
+  {name: "random", price: "1.0", description: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat", stock: "9", rating: 5, supplier_id: 2, user_id: 2},
+  {name: "55", price: "55.0", description: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat", stock: "56", rating: 1, supplier_id: 1, user_id: 2},
+  {name: "555", price: "555.0", description: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat", stock: "3", rating: 5, supplier_id: 1, user_id: 2},
+  {name: "item 1", price: "1.0", description: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat", stock: "67", rating: 5, supplier_id: 1, user_id: 1},
+  {name: "item 2", price: "2.5", description: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat", stock: "6", rating: 5, supplier_id: 2, user_id: 2},
+  {name: "item 3", price: "0.99", description: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat", stock: "70", rating: 4, supplier_id: 2, user_id: 1},
+  {name: "item 4", price: "2.5", description: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat", stock: "45", rating: 1, supplier_id: 2, user_id: 1},
+  {name: "item 5", price: "1.2", description: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat", stock: "21", rating: 4, supplier_id: 1, user_id: 2},
+  {name: "newst item", price: "5.99", description: "this is a new item that i just created with the new models.", stock: "20", rating: 5, supplier_id: 2, user_id: 1},
+  {name: "pro", price: "20.0", description: "orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat", stock: "20", rating: 5, supplier_id: 2, user_id: 2},
+  {name: "TEST USER LINK", price: "9.75", description: "this is to test user creation of products", stock: "8", rating: 3, supplier_id: 1, user_id: 2},
+  {name: "bmaljai", price: "6.0", description: "test description", stock: "20", rating: 1, supplier_id: 2, user_id: 2},
+  {name: "Bejan", price: "500.0", description: "new", stock: "11", rating: 1, supplier_id: 3, user_id: 2}
+])
+Supplier.create!([
+  {name: "Bejan", email: "bmaljai@gmail.com", phone: "5559999"},
+  {name: "Supplier2", email: "ai@gmail.com", phone: "8675309"},
+  {name: "new", email: nil, phone: nil}
 ])
