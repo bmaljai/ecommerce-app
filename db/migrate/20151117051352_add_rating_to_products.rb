431c7e5a25de6a7d445e3ff6b8c77e12767a3fa2
Bejan Maljai <bmaljai@gmail.com>
2015-11-25 18:07:11 -0800

    adjustment for postgresql migration

--- db/migrate/20151117051352_add_rating_to_products.rb
+++ db/migrate/20151117051352_add_rating_to_products.rb
@@ -1,7 +1,7 @@
 class AddRatingToProducts < ActiveRecord::Migration
   def change
     add_column :products, :rating, :integer
-    change_column :products, :price, :decimal, precision: 6, scale: 2
+    change_column :products, :price, 'numeric USING price::numeric(6,2)'
     change_column :products, :description, :text
   end
 end
